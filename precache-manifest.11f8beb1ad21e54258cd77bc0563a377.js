2cc89634390b8e0337e3a48e3a1870a0366d503f
NKodos <kodoses@gmail.com>
2022-06-20 21:40:16 +0300

    update

---
+++ precache-manifest.11f8beb1ad21e54258cd77bc0563a377.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b07fadb75a9072ea195c6c64b754d94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b244bccdc60ec308391b",
+    "url": "/static/css/main.57cdc71d.chunk.css"
+  },
+  {
+    "revision": "16749f34cbe92cffb92a",
+    "url": "/static/js/2.ab8386f1.chunk.js"
+  },
+  {
+    "revision": "b21c0b070fbf389a149e773557d68a8a",
+    "url": "/static/js/2.ab8386f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b244bccdc60ec308391b",
+    "url": "/static/js/main.af9dff28.chunk.js"
+  },
+  {
+    "revision": "53570ca2586dae90fa4f",
+    "url": "/static/js/runtime-main.aa027e55.js"
+  },
+  {
+    "revision": "0bbfc705e37a927ce2ae72b749b3154d",
+    "url": "/static/media/Simple-Line-Icons.0bbfc705.woff"
+  },
+  {
+    "revision": "6ac7320f709ffd2784b4a861e5b60395",
+    "url": "/static/media/Simple-Line-Icons.6ac7320f.woff2"
+  },
+  {
+    "revision": "b2892aa62b0fb2c21c8d7700e2ef6e56",
+    "url": "/static/media/Simple-Line-Icons.b2892aa6.ttf"
+  },
+  {
+    "revision": "dacd16c9a3d2cb61b472a35979015bf3",
+    "url": "/static/media/Simple-Line-Icons.dacd16c9.eot"
+  },
+  {
+    "revision": "de8ad55bfeaebbe67c5b9a362749de1a",
+    "url": "/static/media/Simple-Line-Icons.de8ad55b.svg"
+  },
+  {
+    "revision": "d64de9f2327ac538eb37592d802612e7",
+    "url": "/static/media/about-me.d64de9f2.jpg"
+  },
+  {
+    "revision": "33d0b67c047e466b46c464b8eefc2d31",
+    "url": "/static/media/custom-select.33d0b67c.png"
+  },
+  {
+    "revision": "f6dc099240cc766d0ba0fcacb7b1a74d",
+    "url": "/static/media/danger.f6dc0992.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c2a3ffca7630461041c76d406bfb8fef",
+    "url": "/static/media/goal1.c2a3ffca.jpg"
+  },
+  {
+    "revision": "694740e383ebad0e134e2a42d6d3431f",
+    "url": "/static/media/goal2.694740e3.jpg"
+  },
+  {
+    "revision": "ab4456d0717e10245408ba95b6274d4c",
+    "url": "/static/media/goal3.ab4456d0.jpg"
+  },
+  {
+    "revision": "a871b4e044936afee14307eba3b04c28",
+    "url": "/static/media/module-image1.a871b4e0.jpg"
+  },
+  {
+    "revision": "2522fbe95a253c9fb4000312646e3512",
+    "url": "/static/media/module-image2.2522fbe9.jpg"
+  },
+  {
+    "revision": "e3cac14c40a94a0c361ce8342e24d915",
+    "url": "/static/media/module-image3.e3cac14c.jpg"
+  },
+  {
+    "revision": "1ba7051dfeb95a548cbe6dee3289fb80",
+    "url": "/static/media/peculiarities1.1ba7051d.jpg"
+  },
+  {
+    "revision": "952f2dba5f512b1922bfbf6d5036f8d3",
+    "url": "/static/media/peculiarities2.952f2dba.jpg"
+  },
+  {
+    "revision": "4f91d49e9fae8f3e7abb0b936dfa30ea",
+    "url": "/static/media/peculiarities3.4f91d49e.jpg"
+  },
+  {
+    "revision": "de4ee7ad58f75b356c97fa17653567c7",
+    "url": "/static/media/success.de4ee7ad.svg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/static/media/themify.cba68f98.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "7a3f95cec4b5847d44d1e08576dc921b",
+    "url": "/static/media/warning.7a3f95ce.svg"
+  },
+  {
+    "revision": "0dbf89cd67b744c43c6c9d9dd838a05d",
+    "url": "/static/media/white-text.0dbf89cd.png"
+  }
+]);
